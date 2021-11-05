3c7b24d4e15951e793418c2f206c913d7aa0d062
Suzukihonoka <sakurafirex@gmail.com>
2021-11-05 16:37:02 +0800

    some fill up and optimization
    * add "bad request" error

--- internal/errors/api.go
+++ internal/errors/api.go
@@ -3,6 +3,7 @@ package errors
 import "errors"
 
 var (
+	BadRequest                   = errors.New("bad request")
 	SessionPoolMaxReached        = errors.New("session pool max reached")
 	SessionInvalid               = errors.New("session invalid")
 	NotificationsListParseFailed = errors.New("notifications list parse failed")
